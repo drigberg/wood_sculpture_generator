ffc195e5031243c92e41fc72f5d79af584d8ba04
Daniel Rigberg <daniel.rigberg@juno.care>
2023-11-15 12:24:51 +0100

    even distribution + blur

---
+++ README.md
@@ -0,0 +1,15 @@
+# wood_sculpture_pattern
+
+This tool generates patterns to be used for "organic-looking", net-like wood sculptures.
+
+## Installation
+
+```
+python3 -m pipenv install
+```
+
+## Usage
+
+```
+python3 -m pipenv run main.py [EVEN]
+```
--- main.py
+++ main.py
@@ -3,17 +3,30 @@ import math
 import random
 import sys
 
+from scipy.ndimage.filters import gaussian_filter
 from matplotlib import cm
 import numpy as np
 from PIL import Image
 
-DEFAULT_CONFIG = {
+RANDOM_CONFIG = {
     "distribution": "RANDOM",
+    "color": True,
+    "blur": 10,
+    "width": 500,
+    "height": 300,
+    "density": 0.2,
+    "maxCircleRadius": 0.2,
+    "minCircleRadius": 0.1
+}
+
+EVEN_CONFIG = {
+    "distribution": "RANDOM",
+    "blur": 15,
     "color": True,
     "width": 500,
     "height": 300,
-    "density": 0.1,
-    "maxCircleRadius": 0.2,
+    "density": 0.8,
+    "maxCircleRadius": 0.1,
     "minCircleRadius": 0.05
 }
 
@@ -27,9 +40,8 @@ class Circle:
         return f"x={self.x}, y={self.y}, radius={self.radius}"
 
 class Sculptor:
-    def load_config(self):
-        self.config = DEFAULT_CONFIG
-        return self.config
+    def __init__(self, config):
+        self.config = config
 
     def create_field(self) -> list[list[float]]:
         self.field = [
@@ -44,10 +56,9 @@ class Sculptor:
             for y in range(max(0, circle.y - circle.radius), min(self.config['height'], circle.y + circle.radius)):
                 distance = math.sqrt((circle.x - x) ** 2 + (circle.y - y) ** 2)
                 if distance < circle.radius:
-                    self.field[y][x] += 1 - distance / circle.radius 
+                    self.field[y][x] += 1 - distance / circle.radius
 
     def place_circles(self):
-        print()
         min_side = min(self.config['width'], self.config['height'])
         num_circles = min_side * self.config['density']
         min_radius = int(min_side * self.config['minCircleRadius'])
@@ -61,18 +72,59 @@ class Sculptor:
                     radius=random.randrange(min_radius, max_radius)
                 )
                 self.apply_circle(circle)
+        elif self.config["distribution"] == "EVEN":
+            # See: https://stackoverflow.com/questions/27499139/how-can-i-set-a-minimum-distance-constraint-for-generating-points-with-numpy-ran
+            # specify params
+            n = num_circles
+            shape = np.array([self.config["height"], self.config["width"]])
+            sensitivity = self.config["density"]
+
+            # compute grid shape based on number of points
+            width_ratio = shape[1] / shape[0]
+            num_y = np.int32(np.sqrt(n / width_ratio)) + 1
+            num_x = np.int32(n / num_y) + 1
+
+            # create regularly spaced neurons
+            x = np.linspace(0., shape[1]-1, num_x, dtype=np.float32)
+            y = np.linspace(0., shape[0]-1, num_y, dtype=np.float32)
+            coords = np.stack(np.meshgrid(x, y), -1).reshape(-1,2)
+
+            # compute spacing
+            init_dist = np.min((x[1]-x[0], y[1]-y[0]))
+            min_dist = init_dist * (1 - sensitivity)
+
+            assert init_dist >= min_dist
+
+            # perturb points
+            max_movement = (init_dist - min_dist)/2
+            noise = np.random.uniform(
+                low=-max_movement,
+                high=max_movement,
+                size=(len(coords), 2))
+            coords += noise
+            for coord in coords:
+                circle = Circle(
+                    x=int(coord[0]),
+                    y=int(coord[1]),
+                    radius=random.randrange(min_radius, max_radius)
+                )
+                self.apply_circle(circle)
+
+                
         else:
-            print('NOPE')
+            raise Exception(f"Unrecognized distribution: {self.config['distribution']}")
         
+    def soften(self):
+        self.field = gaussian_filter(self.field, sigma=self.config['blur'])
+
+
     def generate(self):
-        self.load_config()
         self.create_field()
         self.place_circles()
+        self.soften()
         return self.field
     
     def print(self):
-        print()
-
         if self.config['color'] == True:
             array = np.uint8(cm.gist_earth(self.field) * 255)
         else:
@@ -81,11 +133,12 @@ class Sculptor:
             
         image = Image.fromarray(array)
         image.save('output/temp.png')
-        print()
 
 if __name__ == '__main__':
     print("Running...")
-    sculptor = Sculptor()
+
+    config = EVEN_CONFIG if len(sys.argv) > 1 and sys.argv[1] == 'EVEN' else RANDOM_CONFIG
+    sculptor = Sculptor(config)
     sculptor.generate()
     sculptor.print()
     print("Done!")
